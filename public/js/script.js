ea51486731ad3651a9b6785f96df74915f9d1d4b
Eddy de Lange <eddydelange@live.nl>
2017-03-07 14:38:51 +0100

    check everything
    now you can check your code in mastermind and its says incorrect or
    correct also code is beter

--- public/js/script.js
+++ public/js/script.js
@@ -26,15 +26,15 @@ function chooseLevel() {
     }
 }
 
-
 //Load html for the game
 function loadHtml(colors, levelGame) {
     //creating var's
     var colorsChoses = '';
     var answersUser = '';
     var whenBreak = 0;
+    var columnsCode = 10;
     //Creates 10 rows
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < columnsCode; i++) {
         //How much rows 2,3....
         answersUser += '<tr>';
             for (var t = 0; t < levelGame; t++) {
@@ -59,32 +59,79 @@ function loadHtml(colors, levelGame) {
     document.getElementById("gameTable").innerHTML = answersUser;
 
     //Calls function
-    clickColors(colors, levelGame);
+    var codeToGuess = createCode(colors, levelGame);
+    clickColors(colors, levelGame, codeToGuess, columnsCode);
 }
 
+//Create code to guess by the user
+function createCode(colors, levelGame) {
+    var newCode = [];
+    for (var g = 0; g < levelGame; g++) {
+        //Get random value out array en push it in other aray
+        newCode.push(colors[Math.floor(Math.random() * colors.length)]);
+    }
+    return newCode;
+}
 
-function clickColors(colors, levelGame) {
+//Check code user with code computer
+function checkCode(codeToGuess, chosenColor) {
+    console.log(chosenColor);
+    for(var placeVal = codeToGuess.length; placeVal--;) {
+        if(codeToGuess[placeVal] !== chosenColor[placeVal])
+            return false;
+    }
+    return true;
+}
+
+function clickColors(colors, levelGame, codeToGuess, columnsCode) {
     var columns = 0;
     var rows = 0;
+    var paragraphText = document.createElement('h2');
+    var chosenColor = [];
+
+    console.log(codeToGuess);
     //Foreach color in array creating click function
     colors.forEach(function(color) {
         document.getElementById(color).onclick = function() {
-            if (rows < levelGame) {
-                var elementId = "columns"+columns+"rows"+rows;
-                var currElement = document.getElementById(elementId);
-                //Changes classname
-                currElement.className = color;
-            } else {
-                columns++
-                rows = 0;
 
-                //Er komt hier iets anders alleen dit is effe snel
+            //check if your dont cross the limit of columns
+            if (columns < columnsCode) {
+
                 var elementId = "columns"+columns+"rows"+rows;
                 var currElement = document.getElementById(elementId);
                 //Changes classname
                 currElement.className = color;
+                //add chosen color in an array
+                chosenColor.push(color);
+                rows++
+
+                if (rows == levelGame) {
+                    correctcode = checkCode(codeToGuess, chosenColor);
+                    //Correct code
+                    if (correctcode == true) {
+                        //function nodig
+                        location.href = "http://localhost/Jaar%202/mastermind/mastermind/index";
+
+                    } else { //Inorrect code
+                        
+                        //empty array
+                        chosenColor = [];
+                        //Add text to element
+                        paragraphText.textContent = 'Incorrect';
+                        document.getElementById("gameTable").appendChild(paragraphText);
+
+                        //new column
+                        columns++
+                        rows = 0;
+
+                        if (columnsCode == columns) {
+                            //Add text to element
+                            paragraphText.textContent = 'Game-Over';
+                            document.getElementById("gameTable").appendChild(paragraphText);
+                        }
+                    }
+                }
             }
-            rows++
         }
     });
 }
