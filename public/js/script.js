9a700221ddde137af86a6d8f602fb5800ed16168
EddydeLange <eddydelange@live.nl>
2017-03-08 18:47:32 +0100

    Mastermind game
    functions of the game works now it says if you failed or not and you can
    see what color is correct

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,27 @@
-//Choose level
-function chooseLevel() {
-    //Array with all the colors
-    var colors=['lightBlue','red','green','purple','yellow'];
+
+function loadHtml() {
+    //Get colors
+    colors = colorToChoose();
+    //Creates buttons
+    levelButtons(colors);
+    //Choose level function
+    chooseLevel(colors);
+}
+
+//create html for the game
+function createGame(colors, levelGame) {
+    //How much columns
+    var columnsCode = 10;
+    //Calls functions
+    createRowsColumns(levelGame, columnsCode);
+    createOptionsColors(colors, levelGame)
+    //Creates code to guess
+    var codeToGuess = createCode(colors, levelGame);
+    clickColors(colors, levelGame, codeToGuess, columnsCode);
+}
+
+//Create buttons with level
+function levelButtons(colors) {
     var buttonsChoose = '<h1>level</h1>';
     var levelText = 0;
     //Creates button for the levels
@@ -13,7 +33,11 @@ function chooseLevel() {
     }
     //put in UI
     document.getElementById("colorsToChoose").innerHTML = buttonsChoose;
+}
 
+//Choose level
+function chooseLevel(colors) {
+    //Calls function with the colors
     var buttons = document.getElementsByTagName("button");
     for (var b = 0; b < buttons.length; b++) {
         //Button on click level
@@ -21,28 +45,38 @@ function chooseLevel() {
             //Value in var
             var levelGame = this.value;
             //Calls function and send params
-            loadHtml(colors, levelGame)
+            createGame(colors, levelGame)
         };
     }
 }
 
-//Load html for the game
-function loadHtml(colors, levelGame) {
+function createRowsColumns(levelGame, columnsCode) {
     //creating var's
-    var colorsChoses = '';
     var answersUser = '';
-    var whenBreak = 0;
-    var columnsCode = 10;
     //Creates 10 rows
     for (var i = 0; i < columnsCode; i++) {
         //How much rows 2,3....
         answersUser += '<tr>';
+            //Creates cirkels where color could put in
             for (var t = 0; t < levelGame; t++) {
                 var ColumnsRows = 'columns'+ i +'rows' + t +'';
                 answersUser += '<td id="'+ ColumnsRows +'" class="table-responsive"></td>';
             }
+            //Creates td with error if position color is correct or not
+            for (var a = 0; a < levelGame; a++) {
+                var ColumnsError = 'columns'+ i +'rowsError' + a +'';
+                answersUser += '<td id="'+ ColumnsError +'" class="smallCircles table-responsive"></td>';
+            }
         answersUser += '</tr>';
     }
+    //Put in UI
+    document.getElementById("gameTable").innerHTML = answersUser;
+}
+
+function createOptionsColors(colors, levelGame) {
+    //creating var's
+    var colorsChoses = '';
+    var whenBreak = 0;
     //Creates for every color in array an own td
     colors.forEach(function(color) {
         whenBreak++
@@ -56,11 +90,89 @@ function loadHtml(colors, levelGame) {
     });
     //Put in UI
     document.getElementById("colorsToChoose").innerHTML = colorsChoses;
-    document.getElementById("gameTable").innerHTML = answersUser;
+}
 
-    //Calls function
-    var codeToGuess = createCode(colors, levelGame);
-    clickColors(colors, levelGame, codeToGuess, columnsCode);
+//Check code user with code computer
+function checkCode(codeToGuess, chosenColor, columns) {
+    var errorsCode = 0;
+    var correctcode = true;
+    for(var placeVal = codeToGuess.length; placeVal--;) {
+        //put id in an val
+        var errorElementId = 'columns'+columns+'rowsError'+errorsCode+'';
+        //find element with the id
+        var errorElement = document.getElementById(errorElementId);
+
+        if(codeToGuess[placeVal] !== chosenColor[placeVal]) {
+            correctcode = false;
+            //search for value if it exists
+            var exists = codeToGuess.indexOf(chosenColor[placeVal]);
+
+            if (exists !== -1) {
+                //Give element an class
+                errorElement.className += " errorRed";
+                errorsCode++
+            }
+        } else if(codeToGuess[placeVal] == chosenColor[placeVal]) {
+            errorElement.className += " errorWhite";
+            errorsCode++
+        }
+    }
+    return correctcode;
+}
+
+function clickColors(colors, levelGame, codeToGuess, columnsCode) {
+    var columns = 0;
+    var rows = 0;
+    var paragraphText = document.createElement('h2');
+    var chosenColor = [];
+
+
+    //Foreach color in array creating click function
+    colors.forEach(function(color) {
+        document.getElementById(color).onclick = function() {
+            //check if your dont cross the limit of columns
+            if (columns < columnsCode) {
+                var elementId = "columns"+columns+"rows"+rows;
+                //Changes classname
+                var currElement = document.getElementById(elementId);
+                currElement.className += " "+color;
+                //empty text with error
+                paragraphText.textContent = '';
+                //add chosen color in an array
+                chosenColor.push(color);
+                rows++
+
+                if (rows == levelGame) {
+                    correctcode = checkCode(codeToGuess, chosenColor, columns);
+                    //Correct code
+                    if (correctcode == true) {
+                        //function nodig
+                        paragraphText.textContent = 'Correct';
+                    } else { //Inorrect code
+                        //empty array
+                        chosenColor = [];
+                        //Add text to element
+                        paragraphText.textContent = 'Incorrect';
+                        //new column
+                        columns++
+                        rows = 0;
+
+                        if (columnsCode == columns) {
+                            //Add text to element
+                            paragraphText.textContent = 'Game-Over';
+                        }
+                    }
+                    document.getElementById("gameTable").appendChild(paragraphText);
+                }
+            }
+        }
+    });
+}
+
+//Colors
+function colorToChoose(colors, levelGame) {
+    var colors=['lightBlue','red','green','purple','yellow','pink','orange','lightGreen'];
+    return colors;
 }
 
 //Create code to guess by the user
@@ -70,70 +182,8 @@ function createCode(colors, levelGame) {
         //Get random value out array en push it in other aray
         newCode.push(colors[Math.floor(Math.random() * colors.length)]);
     }
+    console.log(newCode);
     return newCode;
 }
 
-//Check code user with code computer
-function checkCode(codeToGuess, chosenColor) {
-    console.log(chosenColor);
-    for(var placeVal = codeToGuess.length; placeVal--;) {
-        if(codeToGuess[placeVal] !== chosenColor[placeVal])
-            return false;
-    }
-    return true;
-}
-
-function clickColors(colors, levelGame, codeToGuess, columnsCode) {
-    var columns = 0;
-    var rows = 0;
-    var paragraphText = document.createElement('h2');
-    var chosenColor = [];
-
-    console.log(codeToGuess);
-    //Foreach color in array creating click function
-    colors.forEach(function(color) {
-        document.getElementById(color).onclick = function() {
-
-            //check if your dont cross the limit of columns
-            if (columns < columnsCode) {
-
-                var elementId = "columns"+columns+"rows"+rows;
-                var currElement = document.getElementById(elementId);
-                //Changes classname
-                currElement.className = color;
-                //add chosen color in an array
-                chosenColor.push(color);
-                rows++
-
-                if (rows == levelGame) {
-                    correctcode = checkCode(codeToGuess, chosenColor);
-                    //Correct code
-                    if (correctcode == true) {
-                        //function nodig
-                        location.href = "http://localhost/Jaar%202/mastermind/mastermind/index";
-
-                    } else { //Inorrect code
-                        
-                        //empty array
-                        chosenColor = [];
-                        //Add text to element
-                        paragraphText.textContent = 'Incorrect';
-                        document.getElementById("gameTable").appendChild(paragraphText);
-
-                        //new column
-                        columns++
-                        rows = 0;
-
-                        if (columnsCode == columns) {
-                            //Add text to element
-                            paragraphText.textContent = 'Game-Over';
-                            document.getElementById("gameTable").appendChild(paragraphText);
-                        }
-                    }
-                }
-            }
-        }
-    });
-}
-
-chooseLevel();
+loadHtml();
